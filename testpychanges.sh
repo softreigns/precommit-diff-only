d259b2086e94261b3623048bfc1879b72c95e271
Komal Singh <softreigns@gmail.com>
2023-08-15 19:03:43 -0700

    Update testpychanges.sh

--- testpychanges.sh
+++ testpychanges.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-mkdir -p .tmpPyCh/dfiles && mkdir -p .tmpPyCh/issues;
-for file in $(git diff --cached --name-only | grep .py); do
-  filename="${file##*/}";
-  git diff --cached "${file}" | grep "^[+][^+-]" | sed "s|^+||g" > .tmpPyCh/dfiles/"${filename}";
-  for issue in "$(flake8 "$file")"; do
-    p1="${issue#*:}" && p2=${p1%%:*};
-    mline=$(sed -n "$p2"p "$file");
-    echo "---$mline---";
-    [ -n "$mline" ] && mt=$(cat ".tmpPyCh/dfiles/${filename}" | grep "$mline");
+mkdir -p .tmpPyCh/dfiles
+mkdir -p .tmpPyCh/issues
+for file in $(git diff --cached --name-only | grep '\.py'); do
+  filename="${file##*/}"
+  git diff --cached "${file}" | grep "^[+][^+-]" | sed "s|^+||g" >.tmpPyCh/dfiles/"${filename}"
+  IFS=$'\n'
+  for issue in $(flake8 "$file"); do
+    p1="${issue#*:}" && p2="${p1%%:*}"
+    mline=$(sed -n "$p2"p "$file")
+    [ -n "$mline" ] && mt=$(grep "$mline" <".tmpPyCh/dfiles/${filename}")
     if [ -n "$mt" ]; then
-      echo -e "==>" >> .tmpPyCh/output.txt;
-      echo "$issue" >> .tmpPyCh/output.txt;
-    fi;
-    unset mt; unset mline;
-  done;
+      echo "$issue" >>.tmpPyCh/output.txt
+    fi
+    unset mt
+    unset mline
+  done
 done
 [ -f .tmpPyCh/output.txt ] && (cat .tmpPyCh/output.txt && rm -rf .tmpPyCh && exit 1)
