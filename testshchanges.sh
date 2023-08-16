febd217237704d33dd81bc0da56e52829b70c531
Komal Singh <softreigns@gmail.com>
2023-08-15 19:04:52 -0700

    Update testshchanges.sh

--- testshchanges.sh
+++ testshchanges.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-mkdir -p .tmpShCh/dfiles && mkdir -p .tmpShCh/issues;
-for file in $(git diff --cached --name-only | grep .sh); do
-  filename="${file##*/}";
+mkdir -p .tmpShCh/dfiles && mkdir -p .tmpShCh/issues
+for file in $(git diff --cached --name-only | grep '\.sh'); do
+  filename="${file##*/}"
   #git diff --cached --color=always -- "${file}" | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/' >.tmpShCh/dfiles/"${filename}"
-  git diff --cached "${file}" | grep "^[+][^+-]" | sed "s|^+||g" > .tmpShCh/dfiles/"${filename}";
-  shellcheck "$file" | sed "/^$/d" | awk "/^In/{x=\".tmpShCh/issues/${filename%.*}\"++i;}{print > x;}";
-  for ifile in .tmpShCh/issues/${filename%.*}*; do
-    mline="$(cat "$ifile" | head -2)";
-    [ -n "$mline" ] && mt=$(cat ".tmpShCh/dfiles/${filename}" | grep "$mline");
+  git diff --cached "${file}" | grep "^[+][^+-]" | sed "s|^+||g" >.tmpShCh/dfiles/"${filename}"
+  shellcheck "$file" | sed "/^$/d" | awk "/^In/{x=\".tmpShCh/issues/${filename%.*}\"++i;}{print > x;}"
+  find .tmpShCh/issues -type f -iname "${filename%.*}*" >.tmpShCh/tmp
+  while IFS= read -r ifile; do
+    mline=$(head -2 <"$ifile")
+    [ -n "$mline" ] && mt=$(grep "$mline" <".tmpShCh/dfiles/${filename}")
     if [ -n "$mt" ]; then
-      echo -e "\n==>\n" >> .tmpShCh/output.txt;
-      cat "$ifile" >> .tmpShCh/output.txt;
-    fi;
-    unset mt; unset mline;
-  done;
+      echo -e "\n==>\n" >>.tmpShCh/output.txt
+      cat "$ifile" >>.tmpShCh/output.txt
+    fi
+    unset mt
+    unset mline
+  done <.tmpShCh/tmp
 done
-[ -f .tmpShCh/output.txt ] && (cat .tmpShCh/output.txt && exit 1)
+[ -f .tmpShCh/output.txt ] && (cat .tmpShCh/output.txt && rm -rf .tmpShCh exit 1)
